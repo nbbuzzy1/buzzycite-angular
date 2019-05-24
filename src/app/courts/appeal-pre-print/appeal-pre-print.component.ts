8124916d6623d899af25a4854826282ba071ce6c
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2019-05-23 22:32:26 -0400

    added services to appeal-pre-print component

--- src/app/courts/appeal-pre-print/appeal-pre-print.component.ts
+++ src/app/courts/appeal-pre-print/appeal-pre-print.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Inputs } from '../../inputs.service'
 
 @Component({
   selector: 'app-appeal-pre-print',
   templateUrl: './appeal-pre-print.component.html',
   styleUrls: ['./appeal-pre-print.component.sass']
 })
-export class AppealPrePrintComponent implements OnInit {
+export class AppealPrePrintComponent {
+  citation: string;
+  italicizedCitation: string;
+  remainingCitation: string;
 
-  constructor() { }
+  constructor(private inputsService: Inputs) {}
 
-  ngOnInit() {
+  createCitation() {
+    //i.e. Smith v. Smith, 234 Ohio App.3d 45, 46-47, 423 N.E.3d 87 (8th Dist.2002)
+    this.pinpointDisplay()
+    this.italicizedCitation = `${this.inputsService.partyOne} v. ${this.inputsService.partyTwo}`;
+    this.remainingCitation = `, ${this.inputsService.ohioVolume} ${this.inputsService.ohioReporter} ${this.inputsService.ohioFirstPage}${this.inputsService.pinpointDisplay}, ${this.inputsService.regionalVolume} ${this.inputsService.regionalReporter} ${this.inputsService.regionalFirstPage} (${this.inputsService.district} Dist.${this.inputsService.year})`;
+    this.citation = this.italicizedCitation + this.remainingCitation
+  }
+
+  pinpointDisplay() {
+    if (this.inputsService.pinpoint) {
+      this.inputsService.pinpointDisplay = `, ${this.inputsService.pinpoint}`
+    } else {
+      this.inputsService.pinpointDisplay = ''
+    }
   }
 
 }
